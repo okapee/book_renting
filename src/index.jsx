58fcdb48b665c77cb2aa15b9dc361f0986b93c7c
Masahiro Okazaki <okazaki.masahiro@fastaccounting.co.jp>
2022-04-11 10:12:36 +0900

    Amplifyのnode-npmバージョンの修正とtypescriptのインストール

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 import { Authenticator, useAuthenticator, withAuthenticator } from '@aws-amplify/ui-react';
-import config from './aws-exports';
 Amplify.configure(config);
 // import { StrictMode } from "react";
 
