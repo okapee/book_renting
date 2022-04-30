427611f45307d95b19fbed398e32e8f966d06d00
Masahiro Okazaki <okazaki.masahiro@fastaccounting.co.jp>
2022-04-30 20:30:04 +0900

    ログイン画面に注記追加

--- src/amplify_login/SignInHeader.js
+++ src/amplify_login/SignInHeader.js
@@ -1,4 +1,5 @@
 import { Heading, useTheme } from '@aws-amplify/ui-react';
+import { Text } from '@chakra-ui/react';
 
 export function SignInHeader() {
   const { tokens } = useTheme();
@@ -6,6 +7,7 @@ export function SignInHeader() {
   return (
     <Heading level={3} padding={`${tokens.space.xl} ${tokens.space.xl} 0`}>
       初めての方はアカウントを作成してください
+      <p className='auth-construction-cmnt'>※Googleアカウントでのログインは現在、実装中です。もう暫くお待ち下さい。</p>
     </Heading>
   );
 }
